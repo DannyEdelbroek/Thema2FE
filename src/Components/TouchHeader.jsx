dacb3786b3d4baec71724adf174799e630f5da0f
Danny Edelbroek <334855@student.mboutrecht.nl>
2026-01-22 20:17:15 +0100

    implement(component): header for contact page

---
+++ src/Components/TouchHeader.jsx
@@ -0,0 +1,16 @@
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+function TouchHeader() {
+    return (
+        <div class="container">
+            <div class="row mb-2 mt-4">
+                <div class="col-12">
+                    <h1 className='mb-2'>Get in <span className='color-red'>touch</span></h1>
+                    <p>Welcome to Global News Wire's contact page! We value your feedback and inquiries. Whether you're a member of the media seeking press information, an advertiser intrested in partnership opportunitie, or an individual witht questions about our subscribtion service, we're here to assist you. Please feel free to reach out to us via phone, email, or contact form below. Our dedicated temas are ready to adress your needds promptly and professionally. Thank you for choosing Global News Wire as your trusted source for reliable news and information.</p>
+                </div>
+            </div>
+        </div>
+    );
+}
+
+export default TouchHeader;
