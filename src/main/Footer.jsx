629d44d27162474404707d0e11b034a8cc4f8678
Danny Edelbroek <334855@student.mboutrecht.nl>
2026-01-22 20:17:33 +0100

    implement(component): footer content

---
+++ src/main/Footer.jsx
@@ -0,0 +1,20 @@
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+function Footer() {
+    return (
+        <></>
+        // <div
+        //     style={{
+        //         backgroundColor: '#f0efef',
+        //         position: 'fixed',
+        //         bottom: 0,
+        //         width: '100%'
+        //     }}
+        //     className="border-top d-flex justify-content-center align-items-center py-2"
+        // >
+        //     Global News Wire | All Rights Reserved
+        // </div>
+    );
+}
+
+export default Footer;
