5e41c16bc56fe2d89ab831cfcec68e53aea6380f
Danny Edelbroek <334855@student.mboutrecht.nl>
2026-01-22 20:54:00 +0100

    update(App): implemented not found 404 with router

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import Navbar from './main/navbar';
 import Footer from './main/Footer';
-// import 'bootstrap/dist/css/bootstrap.min.css';
+import Notfound from  './main/NotFound'
 import { Routes, Route } from 'react-router-dom';
 import Home from './Pages/Home';
 import Contact from './Pages/Contact';
@@ -12,6 +12,7 @@ function App() {
       <Routes>
         <Route path='/' element={<Home />} />
         <Route path='/contact' element={<Contact />} />
+        <Route path='*' element={<Notfound />} />
       </Routes>
       <Footer />
     </>
